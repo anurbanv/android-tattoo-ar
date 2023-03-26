2444fcc5e1e48411b84de6e046b233d567cf689a
Andrius <urban.andrius@gmail.com>
2023-03-26 20:24:18 +0300

    New idea screen template

---
+++ app/src/main/java/com/anurban/android_tattoo_ar/app/feature/idea/new/NewIdeaScreen.kt
@@ -0,0 +1,91 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
+package com.anurban.android_tattoo_ar.app.feature.idea.new
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NewIdeaScreen(
+    modifier: Modifier = Modifier,
+    eventListener: () -> Unit = {},
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "Select style")
+        MyDropDownMenuBox()
+        Text(text = "Describe your tattoo idea")
+        TextField(
+            value = "",
+            onValueChange = { eventListener() },
+        )
+        Button(onClick = { eventListener() }) {
+            Text(text = "Go AI")
+        }
+    }
+}
+
+@Composable
+private fun MyDropDownMenuBox() {
+    val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        TextField(
+            modifier = Modifier.menuAnchor(),
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = { },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }
+        ) {
+            options.forEach { selectionOption ->
+                DropdownMenuItem(
+                    text = {
+                        Text(text = selectionOption)
+                    },
+                    onClick = {
+                        selectedOptionText = selectionOption
+                        expanded = false
+                    },
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NewIdeaScreenPreview() {
+    NewIdeaScreen()
+}
